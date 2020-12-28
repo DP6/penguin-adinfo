304ab5cb465f03a92bb6aa70690ea5b3d384d51e
Lucas Tonetto Firmo <lucasfirmo@hotmail.com>
2020-12-28 17:58:39 -0300

    Adição do módulo de Configuração

---
+++ src/js/models/Config.js
@@ -0,0 +1,47 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.Config = void 0;
+class Config {
+	constructor(jsonConfig) {
+		this._separator = jsonConfig.separator;
+		delete jsonConfig.separator;
+		this._spaceSeparator = jsonConfig.spaceSeparator;
+		delete jsonConfig.spaceSeparator;
+		this._insertTime = jsonConfig.insertTime;
+		delete jsonConfig.insertTime;
+		this._version = jsonConfig.version;
+		delete jsonConfig.version;
+		this._analyticsTool = jsonConfig.ga || jsonConfig.adobe;
+		delete jsonConfig.ga;
+		delete jsonConfig.adobe;
+		this._medias = jsonConfig;
+	}
+	validateConfig() {
+		return !(
+			!this._separator ||
+			!this._spaceSeparator ||
+			!this._insertTime ||
+			!this._version ||
+			!this._analyticsTool
+		);
+	}
+	get separator() {
+		return this._separator;
+	}
+	get spaceSeparator() {
+		return this._spaceSeparator;
+	}
+	get insertTime() {
+		return this._insertTime;
+	}
+	get version() {
+		return this._version;
+	}
+	get analyticsTool() {
+		return this._analyticsTool;
+	}
+	get medias() {
+		return this._medias;
+	}
+}
+exports.Config = Config;
---
+++ src/ts/models/Config.ts
@@ -0,0 +1,60 @@
+export class Config {
+	private _separator: string;
+	private _spaceSeparator: string;
+	private _insertTime: string;
+	private _version: number;
+	private _analyticsTool: { [key: string]: { [key: string]: string[] } };
+	private _medias: { [key: string]: JSON };
+
+	constructor(jsonConfig: { [key: string]: any }) {
+		this._separator = jsonConfig.separator;
+		delete jsonConfig.separator;
+		this._spaceSeparator = jsonConfig.spaceSeparator;
+		delete jsonConfig.spaceSeparator;
+		this._insertTime = jsonConfig.insertTime;
+		delete jsonConfig.insertTime;
+		this._version = jsonConfig.version;
+		delete jsonConfig.version;
+		this._analyticsTool = jsonConfig.ga || jsonConfig.adobe;
+		delete jsonConfig.ga;
+		delete jsonConfig.adobe;
+		this._medias = jsonConfig;
+	}
+
+	/**
+	 * Checa se a configuração é válida
+	 */
+	public validateConfig(): boolean {
+		return !(
+			!this._separator ||
+			!this._spaceSeparator ||
+			!this._insertTime ||
+			!this._version ||
+			!this._analyticsTool
+		);
+	}
+
+	get separator(): string {
+		return this._separator;
+	}
+
+	get spaceSeparator(): string {
+		return this._spaceSeparator;
+	}
+
+	get insertTime(): string {
+		return this._insertTime;
+	}
+
+	get version(): number {
+		return this._version;
+	}
+
+	get analyticsTool(): { [key: string]: { [key: string]: string[] } } {
+		return this._analyticsTool;
+	}
+
+	get medias(): { [key: string]: JSON } {
+		return this._medias;
+	}
+}
