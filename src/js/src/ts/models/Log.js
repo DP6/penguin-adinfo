1a09dfcb0d8eb7cac5027b716c0dcae22dde60fd
Lucas Tonetto Firmo <lucasfirmo@hotmail.com>
2021-01-11 14:36:34 -0300

    Inclusao do modulo de escrita de logs no GCP

---
+++ src/js/src/ts/models/Log.js
@@ -0,0 +1,5 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.Log = void 0;
+class Log {}
+exports.Log = Log;
---
+++ src/ts/models/cloud/LoggingSingleton.ts
@@ -0,0 +1,55 @@
+import { Log } from '../Log';
+import * as credentials from '../../../../config/gcp_key.json';
+import * as bunyan from 'bunyan';
+import { LoggingBunyan } from '@google-cloud/logging-bunyan';
+
+export class LoggingSingleton extends Log {
+	private static _loggingBunyan = new LoggingBunyan({ credentials });
+	private static _loggerName = 'adinfo';
+	private static _infoInstance: bunyan;
+	private static _errorInstance: bunyan;
+	private static _warningInstance: bunyan;
+
+	private constructor() {
+		super();
+	}
+
+	public static logInfo(message: string): void {
+		if (!LoggingSingleton._infoInstance) {
+			LoggingSingleton._infoInstance = bunyan.createLogger({
+				name: LoggingSingleton._loggerName,
+				streams: [
+					{ stream: process.stdout, level: 'info' },
+					this._loggingBunyan.stream('info'),
+				],
+			});
+		}
+		LoggingSingleton._infoInstance.info(message);
+	}
+
+	public static logError(message: string): void {
+		if (!LoggingSingleton._errorInstance) {
+			LoggingSingleton._errorInstance = bunyan.createLogger({
+				name: LoggingSingleton._loggerName,
+				streams: [
+					{ stream: process.stdout, level: 'error' },
+					this._loggingBunyan.stream('error'),
+				],
+			});
+		}
+		LoggingSingleton._errorInstance.info(message);
+	}
+
+	public static logWarning(message: string): void {
+		if (!LoggingSingleton._warningInstance) {
+			LoggingSingleton._warningInstance = bunyan.createLogger({
+				name: LoggingSingleton._loggerName,
+				streams: [
+					{ stream: process.stdout, level: 'warn' },
+					this._loggingBunyan.stream('warn'),
+				],
+			});
+		}
+		LoggingSingleton._warningInstance.info(message);
+	}
+}
