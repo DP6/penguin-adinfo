428f2216c177ef51a5d9874535f5eee940297bc7
Lucas Tonetto Firmo <lucasfirmo@hotmail.com>
2021-01-11 10:31:33 -0300

    Adicao de testes do ParametrizerFactory

--- test/models/ParametrizerFactory.spec.ts
+++ test/models/ParametrizerFactory.spec.ts
@@ -2,9 +2,9 @@ import { expect } from 'chai';
 import { ParametrizerFactory } from '../../src/ts/models/ParametrizerFactory';
 import { Config } from '../../src/ts/models/Config';
 
-describe('PArametrizerFactory', () => {
-	describe('', () => {
-		it('', () => {
+describe('ParametrizerFactory', () => {
+	describe('Verifica se o Factory está criando as classes corretamente', () => {
+		it('Criação do Factory para adobe', () => {
 			const csvLine = {
 				Url: 'www.teste.com.br',
 				'Tipo de Compra': 'cpc',
@@ -23,7 +23,86 @@ describe('PArametrizerFactory', () => {
 					},
 				},
 			});
-			expect(true).to.equal(true);
+			const object = new ParametrizerFactory(csvLine, config).build(
+				'adobe'
+			);
+			expect(object.constructor.name).to.equal('Adobe');
+		});
+		it('Criação do Factory para GA', () => {
+			const csvLine = {
+				Url: 'www.teste.com.br',
+				'Tipo de Compra': 'cpc',
+				Dispositivo: 'desktop e mobile',
+				Bandeira: 'meu Produto',
+				Veículo: 'meuVeículo',
+			};
+			const config = new Config({
+				separator: ':',
+				spaceSeparator: '_',
+				ga: {
+					utm_source: {
+						'Tipo de Compra': ['cpa', 'cpc'],
+						Bandeira: ['/meu\\ ?Produto/'],
+					},
+				},
+			});
+			const object = new ParametrizerFactory(csvLine, config).build('ga');
+			expect(object.constructor.name).to.equal('GA');
+		});
+		it('Criação do Factory para GoogleAds', () => {
+			const csvLine = {
+				Url: 'www.teste.com.br',
+				'Tipo de Compra': 'cpc',
+				Dispositivo: 'desktop e mobile',
+				Bandeira: 'meu Produto',
+				Veículo: 'meuVeículo',
+			};
+			const config = new Config({
+				separator: ':',
+				spaceSeparator: '_',
+				ga: {
+					utm_source: {
+						'Tipo de Compra': ['cpa', 'cpc'],
+						Dispositivo: ['desktop e mobile'],
+					},
+				},
+				googleads: {
+					campanha: 'utm_source',
+					ad: 'utm_campaign',
+				},
+			});
+			const object = new ParametrizerFactory(csvLine, config).build(
+				'googleads'
+			);
+			expect(object.constructor.name).to.equal('GoogleAds');
+		});
+		it('Criação do Factory para FacebookAds', () => {
+			const csvLine = {
+				Url: 'www.teste.com.br',
+				'Tipo de Compra': 'cpc',
+				Dispositivo: 'desktop e mobile',
+				Bandeira: 'meu Produto',
+				Veículo: 'meuVeículo',
+			};
+			const config = new Config({
+				separator: ':',
+				spaceSeparator: '_',
+				ga: {
+					utm_source: {
+						'Tipo de Compra': ['cpa', 'cpc'],
+						Dispositivo: ['desktop e mobile'],
+					},
+				},
+				facebookads: {
+					dynamicValues: 'true',
+					utm_source: '{{ad.name}}',
+					utm_campaign: '{{campaign.name}}',
+				},
+			});
+			const object = new ParametrizerFactory(csvLine, config).build(
+				'facebookads'
+			);
+			expect(object.constructor.name).to.equal('FacebookAds');
 		});
 	});
 });
