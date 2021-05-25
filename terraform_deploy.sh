c2b7af561fceb8fb0a4d0f571434ec2e3b9ec4d1
Gustavo Ueti <gustavo.ueti@gmail.com.br>
2021-05-25 17:26:44 -0300

    fix: :bug: Corrigindo o deploy da aplicação

--- terraform_deploy.sh
+++ terraform_deploy.sh
@@ -7,4 +7,5 @@ terraform init && \
 # terraform state rm 'google_storage_bucket.adinfo_bucket'  &&  \ 
 # terraform state rm 'google_firestore_document.mydoc'  &&  \ 
 # terraform state rm 'google_firestore_document.sub_document'  &&  \ 
+# terraform state rm 'google_app_engine_standard_app_version.adinfo'  &&  \ 
 terraform apply -auto-approve 
