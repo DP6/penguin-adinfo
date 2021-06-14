15e761fc70d82784d102371964f73dc672aaebaa
Gustavo Ueti <gustavo.ueti@gmail.com.br>
2021-06-14 18:45:24 -0300

    feat: :zap: Adição do identificador do separador automático
    Adicionamos com que o código identifique qual separador o arquivo está utilizando para gerar um CSV novo, caso nenhum separador seja configurado no front

--- dist/routes/build.js
+++ dist/routes/build.js
@@ -49,7 +49,16 @@ const build = (app) => {
 				}
 			})
 			.then(() => {
-				const jsonFromFile = CsvUtils_1.CsvUtils.csv2json(fileContent.toString(), companyConfig.csvSeparator);
+				const csvContent = fileContent.toString();
+				const jsonFromFile = CsvUtils_1.CsvUtils.csv2json(
+					csvContent,
+					CsvUtils_1.CsvUtils.identifyCsvSepartor(csvContent.split('\n')[0], companyConfig.csvSeparator)
+				);
+				console.log('separador do config: ' + companyConfig.csvSeparator);
+				console.log(
+					'separador da funcao se passado undefined: ' +
+						CsvUtils_1.CsvUtils.identifyCsvSepartor(csvContent.split('\n')[0], companyConfig.csvSeparator)
+				);
 				const jsonParameterized = new Builder_1.Builder(jsonFromFile, companyConfig, media).build();
 				const configVersion = companyConfig.version;
 				const configTimestamp = DateUtils_1.DateUtils.newDateStringFormat(
