6f2498b8c4292b754730d84125d9864a0b48af20
Susana Nakasato <nakasato.susana@gmail.com>
2021-06-18 18:49:24 -0300

    refactor: :recycle: Refatoracao da classe Config e TemplateExcel

--- src/ts/models/TemplateExcel.ts
+++ src/ts/models/TemplateExcel.ts
@@ -11,19 +11,19 @@ export class TemplateExcel {
 	constructor(config: Config) {
 		this._config = config;
 		this._validationRules = config.validationRules;
-		this._columns = Object.keys(this._validationRules);
-		this._workbook = this.createTemplateExcel();
+		this._columns = config.columnNames;
+		this._workbook = this._createTemplateExcel();
 	}
 
 	/**
 	 * Criar o template em formato Excel .xlsx.
 	 * @returns Workbook Excel com abas 'Template' e 'Validation' populados de acordo com a configuração de validação do AdInfo.
 	 */
-	private createTemplateExcel(): Workbook {
+	private _createTemplateExcel(): Workbook {
 		const workbook = new Workbook();
-		this.addExcelCoverWorksheet(workbook);
-		this.addExcelValidationWorksheet(workbook);
-		this.addExcelTemplateWorksheet(workbook);
+		this._addExcelCoverWorksheet(workbook);
+		this._addExcelValidationWorksheet(workbook);
+		this._addExcelTemplateWorksheet(workbook);
 		return workbook;
 	}
 
@@ -39,7 +39,7 @@ export class TemplateExcel {
 	 * Gerar a aba 'cover' no Excel Template, contendo informações do template, como data de download e versão da configuração.
 	 * @param workbook Objeto Workbook no qual a aba será gerada.
 	 */
-	private addExcelCoverWorksheet(workbook: Workbook): void {
+	private _addExcelCoverWorksheet(workbook: Workbook): void {
 		const coverSheet = workbook.addWorksheet('cover');
 		coverSheet.getCell('A1').value = `Template downloaded on: ${new Date()}`;
 		coverSheet.getCell('A2').value = `Configuration version: ${this._config.version}`;
@@ -49,27 +49,27 @@ export class TemplateExcel {
 	 * Gerar a aba de 'Validation' no Excel Template, com os valores populados para validação.
 	 * @param workbook Objeto Workbook no qual a aba será gerada.
 	 */
-	private addExcelValidationWorksheet(workbook: Workbook): void {
+	private _addExcelValidationWorksheet(workbook: Workbook): void {
 		const validationTemplate = workbook.addWorksheet('validation');
-		this.setExcelWorksheetHeader(validationTemplate);
-		this.setValidationExcelWorksheetValues(validationTemplate);
+		this._setExcelWorksheetHeader(validationTemplate);
+		this._setValidationExcelWorksheetValues(validationTemplate);
 	}
 
 	/**
 	 * Gerar a aba de 'Template' no Excel Template, com os campos e validações populados para uso.
 	 * @param workbook Objeto Workbook no qual a aba será gerada.
 	 */
-	private addExcelTemplateWorksheet(workbook: Workbook): void {
+	private _addExcelTemplateWorksheet(workbook: Workbook): void {
 		const templateSheet = workbook.addWorksheet('template');
-		this.setExcelWorksheetHeader(templateSheet);
-		this.setTemplateExcelWorksheetDataValidation(templateSheet);
+		this._setExcelWorksheetHeader(templateSheet);
+		this._setTemplateExcelWorksheetDataValidation(templateSheet);
 	}
 
 	/**
 	 * Nomear as colunas e inserir os valores dos campos na aba do Excel, com os valores de acordo com a configuração de validação do AdInfo.
 	 * @param worksheet Objeto Worksheet (aba do Excel) no qual terá os campos inseridos e as colunas nomeadas.
 	 */
-	private setExcelWorksheetHeader(worksheet: Worksheet): void {
+	private _setExcelWorksheetHeader(worksheet: Worksheet): void {
 		const columns = ['url', ...this._columns];
 		worksheet.columns = columns.map((column) => {
 			return {
@@ -84,7 +84,7 @@ export class TemplateExcel {
 	 * Inserir o nome do campo e os valores de cada campo que serão usados para validação de dados, de acordo com a configuração de validação do AdInfo.
 	 * @param validationWorksheet Objeto Worksheet (aba do Excel) referente à aba de validação.
 	 */
-	private setValidationExcelWorksheetValues(validationWorksheet: Worksheet): void {
+	private _setValidationExcelWorksheetValues(validationWorksheet: Worksheet): void {
 		this._columns.forEach((column) => {
 			validationWorksheet.getColumn(column).values = [column, ...this._validationRules[column]];
 		});
@@ -94,7 +94,7 @@ export class TemplateExcel {
 	 * Aplicar a validação de dados nos campos do template, de acordo com a configuração de validação do AdInfo, para as primeiras 1000 linhas.
 	 * @param templateWorksheet Objecto worksheet (aba do Excel) referente à aba de template.
 	 */
-	private setTemplateExcelWorksheetDataValidation(templateWorksheet: Worksheet): void {
+	private _setTemplateExcelWorksheetDataValidation(templateWorksheet: Worksheet): void {
 		this._columns.forEach((column) => {
 			if (!StringUtils.isStringForRegex(this._validationRules[column][0])) {
 				const validationCount = this._validationRules[column].length;
