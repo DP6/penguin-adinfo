34b468984b6516efd80d69b7f53fef1557eb6dd5
gustavoueti <gustavoueti@users.noreply.github.com>
2021-12-22 19:58:12 +0000

    style: :lipstick: Apply formatting changes

--- dist/models/DAO/AgencyDAO.js
+++ dist/models/DAO/AgencyDAO.js
@@ -1,35 +1,35 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.AgencyDAO = void 0;
-const FirestoreConnectionSingleton_1 = require("../cloud/FirestoreConnectionSingleton");
+const FirestoreConnectionSingleton_1 = require('../cloud/FirestoreConnectionSingleton');
 class AgencyDAO {
-    constructor() {
-        this._objectStore = FirestoreConnectionSingleton_1.FirestoreConnectionSingleton.getInstance();
-        this._pathToCollection = ['tokens'];
-    }
-    getAllAgenciesFrom(company, agency, userRequestPermission) {
-        return this._objectStore
-            .getCollection(['tokens'])
-            .where('company', '==', company)
-            .get()
-            .then((querySnapshot) => {
-            return this._objectStore.getAllAgenciesFromFirestore(querySnapshot, agency, userRequestPermission);
-        })
-            .catch((err) => {
-            throw err;
-        });
-    }
-    getAllUsersFromAgency(company, agency, userRequestPermission) {
-        return this._objectStore
-            .getCollection(this._pathToCollection)
-            .where('company', '==', company)
-            .get()
-            .then((querySnapshot) => {
-            return this._objectStore.getUsersFromFirestore(querySnapshot, userRequestPermission, agency, false);
-        })
-            .catch((err) => {
-            throw err;
-        });
-    }
+	constructor() {
+		this._objectStore = FirestoreConnectionSingleton_1.FirestoreConnectionSingleton.getInstance();
+		this._pathToCollection = ['tokens'];
+	}
+	getAllAgenciesFrom(company, agency, userRequestPermission) {
+		return this._objectStore
+			.getCollection(['tokens'])
+			.where('company', '==', company)
+			.get()
+			.then((querySnapshot) => {
+				return this._objectStore.getAllAgenciesFromFirestore(querySnapshot, agency, userRequestPermission);
+			})
+			.catch((err) => {
+				throw err;
+			});
+	}
+	getAllUsersFromAgency(company, agency, userRequestPermission) {
+		return this._objectStore
+			.getCollection(this._pathToCollection)
+			.where('company', '==', company)
+			.get()
+			.then((querySnapshot) => {
+				return this._objectStore.getUsersFromFirestore(querySnapshot, userRequestPermission, agency, false);
+			})
+			.catch((err) => {
+				throw err;
+			});
+	}
 }
 exports.AgencyDAO = AgencyDAO;
