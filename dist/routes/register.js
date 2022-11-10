6a3ae75b8b48f4e1fd393ac4511305705edf00be
Lucas Tonetto <lucas.firmo@dp6.com.br>
2022-11-10 18:21:51 -0300

    fix: :bug: Correcao ao cadastrar um usuario admin ou owner sem adOpsTeam
    Corrigido o erro que não permitia o cadastro de um usuario admin ou owner sem o envio da informação de adOpsTeam

--- dist/routes/register.js
+++ dist/routes/register.js
@@ -1,4 +1,35 @@
 'use strict';
+var __awaiter =
+	(this && this.__awaiter) ||
+	function (thisArg, _arguments, P, generator) {
+		function adopt(value) {
+			return value instanceof P
+				? value
+				: new P(function (resolve) {
+						resolve(value);
+				  });
+		}
+		return new (P || (P = Promise))(function (resolve, reject) {
+			function fulfilled(value) {
+				try {
+					step(generator.next(value));
+				} catch (e) {
+					reject(e);
+				}
+			}
+			function rejected(value) {
+				try {
+					step(generator['throw'](value));
+				} catch (e) {
+					reject(e);
+				}
+			}
+			function step(result) {
+				result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+			}
+			step((generator = generator.apply(thisArg, _arguments || [])).next());
+		});
+	};
 Object.defineProperty(exports, '__esModule', { value: true });
 const express_validator_1 = require('express-validator');
 const User_1 = require('../models/User');
@@ -15,62 +46,61 @@ const register = (app) => {
 			.isEmail()
 			.withMessage('Email inválido.'),
 		(0, express_validator_1.body)('password').exists().withMessage('Parâmetro password é obrigatório.'),
-		(req, res) => {
-			const apiResponse = new ApiResponse_1.ApiResponse();
-			let adOpsTeam = req.body.adOpsTeam;
-			const adOpsTeamDAO = new AdOpsTeamDAO_1.AdOpsTeamDAO();
-			if (req.permission === 'AdOpsTeamManager') {
-				adOpsTeam = req.adOpsTeam;
-			}
-			const newUser = new User_1.User(
-				'',
-				req.body.permission,
-				req.advertiser,
-				req.body.email,
-				true,
-				adOpsTeam,
-				req.body.password
-			);
-			if (
-				(req.permission === 'owner' || req.permission === 'admin') &&
-				(req.body.permission === 'user' || req.body.permission === 'adopsteammanager') &&
-				adOpsTeam == false
-			) {
-				const message = 'AdOpsTeam não informado.';
-				apiResponse.responseText = message;
-				apiResponse.errorMessage = message;
-				apiResponse.statusCode = 400;
-				res.status(apiResponse.statusCode).send(apiResponse.jsonResponse);
-				return;
-			}
-			adOpsTeamDAO
-				.getAdOpsTeam(adOpsTeam)
-				.then(() => {
-					const userDAO = new UserDAO_1.UserDAO();
-					userDAO.addUser(newUser);
-				})
-				.then(() => {
-					const message = `Usuário criado para o email ${newUser.email}`;
+		(req, res) =>
+			__awaiter(void 0, void 0, void 0, function* () {
+				const apiResponse = new ApiResponse_1.ApiResponse();
+				let adOpsTeam = req.body.adOpsTeam;
+				const adOpsTeamDAO = new AdOpsTeamDAO_1.AdOpsTeamDAO();
+				if (req.permission === 'AdOpsTeamManager') {
+					adOpsTeam = req.adOpsTeam;
+				}
+				const newUser = new User_1.User(
+					'',
+					req.body.permission,
+					req.advertiser,
+					req.body.email,
+					true,
+					adOpsTeam,
+					req.body.password
+				);
+				if (
+					(req.permission === 'owner' || req.permission === 'admin') &&
+					(req.body.permission === 'user' || req.body.permission === 'adopsteammanager') &&
+					!adOpsTeam
+				) {
+					const message = 'AdOpsTeam não informado.';
 					apiResponse.responseText = message;
-					apiResponse.statusCode = 200;
-				})
-				.catch((err) => {
-					if (err.name === 'AdOpsTeamMissingError') {
-						const message = err.message;
-						apiResponse.responseText = message;
+					apiResponse.errorMessage = message;
+					apiResponse.statusCode = 400;
+					res.status(apiResponse.statusCode).send(apiResponse.jsonResponse);
+					return;
+				}
+				if (adOpsTeam) {
+					yield adOpsTeamDAO.getAdOpsTeam(adOpsTeam).catch((err) => {
+						apiResponse.responseText = err.message;
 						apiResponse.errorMessage = err.message;
 						apiResponse.statusCode = 400;
-					} else {
+						res.status(apiResponse.statusCode).send(apiResponse.jsonResponse);
+					});
+					return;
+				}
+				new UserDAO_1.UserDAO()
+					.addUser(newUser)
+					.then(() => {
+						const message = `Usuário criado para o email ${newUser.email}`;
+						apiResponse.responseText = message;
+						apiResponse.statusCode = 200;
+					})
+					.catch((err) => {
 						const message = 'Falha ao criar permissão!';
 						apiResponse.responseText = message;
 						apiResponse.errorMessage = err.message;
 						apiResponse.statusCode = 500;
-					}
-				})
-				.finally(() => {
-					res.status(apiResponse.statusCode).send(apiResponse.jsonResponse);
-				});
-		}
+					})
+					.finally(() => {
+						res.status(apiResponse.statusCode).send(apiResponse.jsonResponse);
+					});
+			})
 	);
 };
 exports.default = register;
--- src/ts/routes/register.ts
+++ src/ts/routes/register.ts
@@ -1,4 +1,4 @@
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import { User } from '../models/User';
 import { UserDAO } from '../models/DAO/UserDAO';
 import { ApiResponse } from '../models/ApiResponse';
@@ -10,7 +10,7 @@ const register = (app: { [key: string]: any }): void => {
 		body('permission').exists().withMessage('Parâmetro permission é obrigatório.'),
 		body('email').exists().withMessage('Parâmetro email é obrigatório.').isEmail().withMessage('Email inválido.'),
 		body('password').exists().withMessage('Parâmetro password é obrigatório.'),
-		(req: { [key: string]: any }, res: { [key: string]: any }) => {
+		async (req: { [key: string]: any }, res: { [key: string]: any }) => {
 			const apiResponse = new ApiResponse();
 
 			let adOpsTeam = req.body.adOpsTeam;
@@ -33,7 +33,7 @@ const register = (app: { [key: string]: any }): void => {
 			if (
 				(req.permission === 'owner' || req.permission === 'admin') &&
 				(req.body.permission === 'user' || req.body.permission === 'adopsteammanager') &&
-				adOpsTeam == false
+				!adOpsTeam
 			) {
 				const message = 'AdOpsTeam não informado.';
 				apiResponse.responseText = message;
@@ -43,29 +43,28 @@ const register = (app: { [key: string]: any }): void => {
 				return;
 			}
 
-			adOpsTeamDAO
-				.getAdOpsTeam(adOpsTeam)
-				.then(() => {
-					const userDAO = new UserDAO();
-					userDAO.addUser(newUser);
-				})
+			if (adOpsTeam) {
+				await adOpsTeamDAO.getAdOpsTeam(adOpsTeam).catch((err) => {
+					apiResponse.responseText = err.message;
+					apiResponse.errorMessage = err.message;
+					apiResponse.statusCode = 400;
+					res.status(apiResponse.statusCode).send(apiResponse.jsonResponse);
+				});
+				return;
+			}
+
+			new UserDAO()
+				.addUser(newUser)
 				.then(() => {
 					const message = `Usuário criado para o email ${newUser.email}`;
 					apiResponse.responseText = message;
 					apiResponse.statusCode = 200;
 				})
 				.catch((err) => {
-					if (err.name === 'AdOpsTeamMissingError') {
-						const message = err.message;
-						apiResponse.responseText = message;
-						apiResponse.errorMessage = err.message;
-						apiResponse.statusCode = 400;
-					} else {
-						const message = 'Falha ao criar permissão!';
-						apiResponse.responseText = message;
-						apiResponse.errorMessage = err.message;
-						apiResponse.statusCode = 500;
-					}
+					const message = 'Falha ao criar permissão!';
+					apiResponse.responseText = message;
+					apiResponse.errorMessage = err.message;
+					apiResponse.statusCode = 500;
 				})
 				.finally(() => {
 					res.status(apiResponse.statusCode).send(apiResponse.jsonResponse);
